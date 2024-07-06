e98790d4ed5f90d07c722c3025777323d34e6fdb
Peekreatives2023 <peekreatives2022@gmail.com>
2024-07-06 17:24:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,107 @@
+//console.log (`Hello`);
+//console.log (`I like Pizza!`);
+
+//windows.alert (`I like Pizza`);
+
+document.getElementById("MyH1").textContent = `Hello`;
+document.getElementById("MyP").textContent = `I like Pizza!`;
+
+/*let x;
+x = 100;
+console.log (x);
+
+let y = 123
+
+let age = 25;
+let price = 10.99;
+let cgpa = 4.40
+
+console.log(typeof cgpa);
+console.log(`You are ${age} years old`);
+console.log(`The price is $${10.99}`);
+console.log(`My Cgpa is: ${4.4}`);*/
+
+/*let firstName = 'Oluwapelumi'
+let favouriteFood = 'Rice and beans'
+let eMail = 'oluwapelumielizabeth201@gmail.com'
+
+console.log(typeof firstName);
+console.log(firstName);
+console.log(`Your name is ${firstName}`);
+console.log(`I love ${favouriteFood} a lot`);
+console.log(`My email is ${eMail}`);
+
+let online = true;
+console.log(typeof online);
+console.log(`Oluwapelumi is online: ${online}`);
+
+let fullName = "Oluwapelumi";
+let age = 21;
+let student = true;
+
+document.getElementById("P1").textContent = fullName;
+document.getElementById("p2").textContent = age;
+document.getElementById("p3").textContent = student;
+
+document.getElementById("P1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `I am ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${student}`;*/
+
+//let students = 30;
+
+//students = students + 1
+//students = students -1
+//students = students * 2
+//students = students / 2
+//students = students % 1
+//students = students ** 2
+
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+//students **= 2;
+
+//students++;
+//students--;
+//console.log(students);
+
+//let result = 1 + 2 * 3 + 4 ** 2;
+//console.log(result);
+
+/*let username;
+
+username = window.prompt("what's your username?");
+console.log(username);*/
+
+/*let age = window.prompt("How old are you");
+age = Number(age);
+age+=1
+
+console.log(age);
+
+let x = "pizza"
+let y = "pizza"
+let z = "pizza"
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);*/
+
+cconst PI = 3.142
+let radius;
+let circumference;
+
+PI = 5.678;
+
+radius = window.prompt("Enter the radius of a circle");
+radius = Number(radius);
+
+circumference = 2 * pi * radius;
+console.log(circumference);
+
+
