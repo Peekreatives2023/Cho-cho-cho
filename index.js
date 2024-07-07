41bb71d458550e830868a639d390e4d3d022a11f
Peekreatives2023 <peekreatives2022@gmail.com>
2024-07-07 09:38:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -104,8 +104,26 @@ radius = Number(radius);
 circumference = 2 * pi * radius;
 console.log(circumference);*/
 
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
 
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel. textContent = count;
+}
 
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel. textContent = count;
+}
+
+increaseBtn.onclick = function(){
+    count++;
+    countLabel. textContent = count;
+}
 
 
 
